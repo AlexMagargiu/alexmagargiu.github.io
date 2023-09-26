ed99c8184fe3e84451e6fafa7b86f0921a760dea
AlexMagargiu <alexandrumaga2000@gmail.com>
2023-09-26 11:47:14 +0300

    Configuration for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  assetsDir: './src/assets',
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: 'https://alexmagargiu.github.io/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/'
+  }
+
+  return config
 })
